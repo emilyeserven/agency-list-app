5a46b7053634cdcfc107546bc689167bdcbe1cb2
emilyeserven <emily.e.serven@gmail.com>
2017-03-06 14:43:48 -0500

    Added filtering

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # agency-list-app
-
-Please view the site at the [RawGit repository](http://rawgit.com/emilyeserven/agency-list-app/master/index.html#!)!
--- js/controllers/listings.js
+++ js/controllers/listings.js
@@ -2,5 +2,8 @@ angular.module('agencyList');
 agencyList.controller('ListingController', ['$scope', '$http', function($scope, $http) {
     $http.get('./js/agencies.json').then(function(data) {
         $scope.agencies = data.data;
+        $scope.agency_name = "";
+        $scope.agency_category = "";
+        $scope.agency_locations = "";
     });
 }]);
